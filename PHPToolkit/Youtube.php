32d4896bac6575188ece7856a59dea6b23efc345
User <User@DESKTOP-MEFM50E.fibertel.com.ar>
2024-11-16 10:56:51 -0300

    Agregado: funciones getId() y getThumbnails().

--- PHPToolkit/Youtube.php
+++ PHPToolkit/Youtube.php
@@ -25,4 +25,54 @@ class Youtube
     {
         return in_array($domain, $this->domains);
     }
+    
+    /**
+     * Función para obtener el id de un video a partir de su URL.
+     * @param string $url
+     * @return string
+     */
+    public function getId($url)
+    {        
+        $parts = parse_url($url);
+
+        if (!$parts) {
+            return '';
+        }
+        
+        $id = '';
+        $query = '';
+        
+        if ($parts['host'] == 'youtu.be') {
+            $id = trim($parts['path'], '/');
+        } else {
+
+            if ($parts['path'] == '/watch') {
+                parse_str($parts['query'], $query);
+                $id = $query['v'];
+            }
+
+        }
+        
+        return $id;
+    }
+
+    /**
+     * Función para obtener las URLs de los thumbnails de un video.
+     * "0.jpg" corresponde a una imagen grande de 480 x 360 pixels.
+     * Todas las demás son de 120 x 90 pixels.
+     * @param string $id
+     * @return string
+     */
+    public function getThumbnails($id)
+    {
+        $thumbs = array(
+            'default' => "https://img.youtube.com/vi/$id/default.jpg",
+                    0 => "https://img.youtube.com/vi/$id/0.jpg",
+                    1 => "https://img.youtube.com/vi/$id/1.jpg",
+                    2 => "https://img.youtube.com/vi/$id/2.jpg",
+                    3 => "https://img.youtube.com/vi/$id/3.jpg",
+        );
+        
+        return $thumbs;
+    }
 }
